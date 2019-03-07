1b342e821b07420763b8a283a56d95b731e40f26
Your Name <you@example.com>
2019-03-08 01:06:53 +0530

    Form Validation Errors

--- views.py
+++ views.py
@@ -79,21 +79,18 @@ def login_required(test):
 @app.route('/tasks/')
 @login_required
 def tasks():
-    open_tasks = db.session.query(Task) \
-        .filter_by(status='1').order_by(Task.due_date.asc())
-    closed_tasks = db.session.query(Task) \
-        .filter_by(status='0').order_by(Task.due_date.asc())
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
-        open_tasks=open_tasks,
-        closed_tasks=closed_tasks
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
     )
 
 
 @app.route('/add/', methods=['GET', 'POST'])
 @login_required
 def new_task():
+    error = None
     form = AddTaskForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -101,7 +98,7 @@ def new_task():
                 form.name.data,
                 form.due_date.data,
                 form.priority.data,
-                datetime.datetime.now(),
+                datetime.datetime.utcnow(),
                 '1',
                 session['user_id']
             )
@@ -109,10 +106,14 @@ def new_task():
             db.session.commit()
             flash('New entry was successfully posted. Thanks.')
             return redirect(url_for('tasks'))
-        else:
-            flash('All fields are required')
-            return redirect(url_for(tasks))
-    return render_template('tasks.html', form=form)
+    return render_template(
+        'tasks.html',
+        form=form,
+        error=error,
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
+    )
+
 
 
 @app.route('/complete/<int:task_id>/')
@@ -133,3 +134,20 @@ def delete_entry(task_id):
     db.session.commit()
     flash('The task was deleted. Why not add a new one?')
     return redirect(url_for('tasks'))
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text, error), 'error')
+
+
+def open_tasks():
+    return db.session.query(Task).filter_by(
+        status='1').order_by(Task.due_date.asc())
+
+
+def closed_tasks():
+    return db.session.query(Task).filter_by(
+        status='0').order_by(Task.due_date.asc())
