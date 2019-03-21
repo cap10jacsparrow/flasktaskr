0a80b4d1c43302575157d03865f6bbbfc25dfd8d
Your Name <you@example.com>
2019-03-21 15:42:51 +0530

    till 210 - Before new Users table

--- test_file.py
+++ test_file.py
@@ -146,7 +146,6 @@ class AllTests(unittest.TestCase):
         ), follow_redirects=True)
         self.assertIn(b'This field is required.', response.data)
 
-
     def test_users_can_complete_tasks(self):
         self.create_user('Michael', 'michael@realpython.com', 'python')
         self.login('Michael', 'python')
@@ -176,6 +175,37 @@ class AllTests(unittest.TestCase):
         self.assertNotIn(
             b'The task is complete. Nice.', response.data
         )
+        self.assertIn(
+            b'You can only update tasks that belong to you.', response.data
+        )
+
+    def test_users_cannot_delete_tasks_that_are_not_created_by_them(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
+        self.login('Fletcher', 'python101')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.app.get("delete/1/", follow_redirects=True)
+        self.assertIn(
+            b'You can only delete tasks that belong to you.', response.data
+        )
+
+    def test_default_user_role(self):
+        db.session.add(
+            User(
+                "Johnny",
+                "john@doe.com",
+                "johnny"
+            )
+        )
+        db.session.commit()
+        users = db.session.query(User).all()
+        print(users)
+        for user in users:
+            self.assertEquals(user.role, 'user')
 
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -58,8 +58,6 @@ def login():
                 return redirect(url_for('tasks'))
             else:
                 error = 'Invalid username or password.'
-        else:
-            error = 'Both fields are required.'
     return render_template('login.html', form=form, error=error)
 
 
@@ -81,6 +79,7 @@ def logout():
     session.pop('user_id', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
+
 
 @app.route('/tasks/')
 @login_required
@@ -121,25 +120,34 @@ def new_task():
     )
 
 
-
 @app.route('/complete/<int:task_id>/')
 @login_required
 def complete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
-    db.session.commit()
-    flash('The task is complete. Nice.')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id:
+        db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
+        db.session.commit()
+        flash('The task is complete. Nice.')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only update tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete_entry(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).delete()
-    db.session.commit()
-    flash('The task was deleted. Why not add a new one?')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id:
+        db.session.query(Task).filter_by(task_id=new_id).delete()
+        db.session.commit()
+        flash('The task was deleted. Why not add a new one?')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only delete tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 def flash_errors(form):
